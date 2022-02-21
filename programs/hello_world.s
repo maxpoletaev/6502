1e0eb987c485d85ec752f5ac3b481d694bf07337
Max Poletaev <max.poletaev@gmail.com>
2022-02-21 22:02:20 +0300

    Update hello world example

--- programs/hello_world.s
+++ programs/hello_world.s
@@ -1,30 +1,21 @@
 ;XA65
-
 out=$0200
-*=$0300
 
+*=$0300
 loop:
-    lda msg,x    ;copy the message byte after byte to the output buffer
+    lda msg,x     ;copy the message byte after byte to the output buffer
     sta out,x
     inx
-    txa
-    cmp len      ;if we reached the end of the message
-    beq break    ;break the loop
-    jmp loop
+    cpx len       ;while (x < len)
+    bne loop
 
-break:
     jsr flush
-    jmp exit
+    brk
 
 flush:
-    sta $02ff   ;writing to $02ff triggers the output
+    lda #$0A      ;newline
+    sta out+$ff   ;writing to $02ff triggers the output
     rts
-
-exit:
-    jmp exit    ;there is no real way to exit, so we just halt
-                ;please note that this will take 100% of the host CPU
-
-.dsb 16,$00
 
 msg: .asc  "Hello, 6502!"
 len: .byte 12
